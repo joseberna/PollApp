5eeb34292d079a3ae1f481a4a8151081966a7d04
joseberna <josefbernam@gmail.com>
2014-04-28 21:27:55 -0500

    grupos
    se sube con grupos ok

--- js/noticias.js
+++ js/noticias.js
@@ -2,12 +2,17 @@
 $(function()
 {		
 
-	getDataTweets();
+	var maxIdGlobal = 0;
+	var countTweeets = 10;
 
-	function getDataTweets()
+	$('#linkLoadMore').click(loadMore);
+
+	getDataTweets(countTweeets, 0);
+
+	function getDataTweets(count, maxId)
 	{
-
-		var apiUrl = "https://1-dot-logical-light-488.appspot.com/_ah/api/socialnetworkendpoint/v1/getAllTweets"; 
+				
+		var apiUrl = "https://1-dot-logical-light-488.appspot.com/_ah/api/socialnetworkendpoint/v1/getAllTweetsPagination?count=" + count + "&maxId="+ maxId;
 
 		$.ajax({ 
 		 url: apiUrl, 
@@ -18,31 +23,42 @@ $(function()
 		   loadTweets(data);
 		 }, 
 		 error: function(xhr, ajaxOptions, thrownError) { 		   
-		   console.log("Error: "+ + xhr.status);
+		   alert("Error: "+ + xhr.status);
 		 }   
 		});
 
 	}
 
 	function loadTweets(data)
-	{		
+	{					
+		if (data.tweets.length > 0) 
+		{
 
-		$.each(data.tweets, function(index, tweet) {
-			
-			var post = getTemplate(tweet.name, tweet.screenName, tweet.text, tweet.profileImageURL, tweet.createdAt);
+			$.each(data.tweets, function(index, tweet) {
 
-			$('.posts').prepend($(post).fadeIn(function()
-					{
-						$(this).css('display', 'inline-block');
-					}));
+				if (--maxIdGlobal != tweet.id)
+				{
+				
+					post = getTemplate(tweet.name, tweet.screenName, tweet.text, tweet.profileImageURL, tweet.createdAt);			
 
+					maxIdGlobal = tweet.id;
 
-		});		
+					$('.posts').append($(post).fadeIn(1000,function()
+							{
+								$(this).css('display', 'inline-block');
+							}));
+
+				}
+
+			});		
+		}
 
 	}	
 
 	function getTemplate(name, screenName, text, image, createdAt)
 	{
+
+		var date = parseTwitterDate(createdAt);
 
 		var template = '<article class="post"> \
 					<div class="description"> \
@@ -55,11 +71,64 @@ $(function()
 							<p class="text"> \
 								'+text+' \
 								</p> \
+							<p class="date"><strong>'+date+'</strong> </p> \
 						</div> \
 					</div> \
 					</article>';
 
 		return template;
 	}
+
+	function parseTwitterDate(tdate) {
+
+	    var system_date = new Date(Date.parse(tdate));
+	    var user_date = new Date();
+
+	    if (K.ie) {
+	        system_date = Date.parse(tdate.replace(/( \+)/, ' UTC$1'))
+	    }
+
+	    var diff = Math.floor((user_date - system_date) / 1000);
+	    if (diff <= 1) {return "ahora";}
+	    if (diff < 20) {return "Hace "+ diff + " segundos";}
+	    if (diff < 40) {return "Hace medio minuto";}
+	    if (diff < 60) {return "Menos de un minuto";}
+	    if (diff <= 90) {return "Hace un minuto";}
+	    if (diff <= 3540) {return "Hace " + Math.round(diff / 60) + " minutos";}
+	    if (diff <= 5400) {return "Hace 1 Hora";}
+	    if (diff <= 86400) {return "Hace " + Math.round(diff / 3600) + " horas";}
+	    if (diff <= 129600) {return "Hace 1 dia";}
+	    if (diff < 604800) {return "Hace " + Math.round(diff / 86400) + " dias";}
+	    if (diff <= 777600) {return "Hace 1 semana";}
+
+	    return convertDate(system_date);
+	}
+
+	// from http://widgets.twimg.com/j/1/widget.js
+	var K = function () {
+    	var a = navigator.userAgent;
+
+    	return {
+        	ie: a.match(/MSIE\s([^;]*)/)
+    	}
+	}();
+
+	function convertDate(inputFormat) {
+  		function pad(s) { return (s < 10) ? '0' + s : s; }
+  		var monthNames = [ "Ene", "Feb", "Mar", "Abr", "Mayo", "Jun",
+    		"Jul", "Agos", "Sep", "Oct", "Nov", "Dic" ];
+  		var d = new Date(inputFormat);
+  		return pad(d.getDate()) + " " + monthNames[d.getMonth()] + " " + d.getFullYear();
+	}
+
+	function loadMore(ev){
+		console.log("entro al evento = " + maxIdGlobal);
+
+		ev.preventDefault();
+
+		getDataTweets(countTweeets, ++maxIdGlobal);
+
+	}
+
 
 });
